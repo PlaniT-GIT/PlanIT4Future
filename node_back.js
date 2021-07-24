eebdaea1b5fb06d5898004fa4c8a89b03434f426
SanjayKH-Git <sanjayhegde92@gmail.com>
2021-07-24 15:27:22 +0530

    Enter

--- node_back.js
+++ node_back.js
@@ -16,6 +16,6 @@ var server=app.listen(8081, function()
 {
 	var host = server.address().address;
 	var port = server.address().port;
-	console.log("PlanIt app listening at http://%s:%s",host,port);
+	console.log("PlanIt app listening at http://%s:%s",host,port);	
 	console.log(__dirname);
 }); 
